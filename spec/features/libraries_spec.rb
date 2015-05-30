8637daae7c6940aef7f12e75443512de29b6e775
Stefan Rush <stefanrush@gmail.com>
2015-05-30 15:29:06 -0400

    Add feature specs for testing sorters

--- spec/features/libraries_spec.rb
+++ spec/features/libraries_spec.rb
@@ -1,24 +1,23 @@
 require 'spec_helper'
 
-RSpec.describe "Libraries", type: :feature do
+RSpec.describe "Libraries", type: :feature, js: true do
   def random_category
-    Category.find(rand(Category.count) + 1)
+    Category.all[rand(Category.count)]
   end
 
   def library_items
     all('section.libraries ol.list li')
   end
 
-  before(:all) do
+  before(:each) do
     3.times  { create(:category) }
-    20.times { create(:library, category: random_category) }
+    10.times { create(:library, category: random_category) }
+    visit root_path
   end
 
-  describe "filter", js: true do
+  describe "filter" do
     describe "by category" do
-      it "filters libraries by category" do
-        visit root_path
-        
+      it "filters libraries by category" do        
         Category.by_position.each do |category|
           library_count = Library.where(category: category).count
           click_link category.name
@@ -31,21 +30,52 @@ RSpec.describe "Libraries", type: :feature do
     end
   end
 
-  describe "sorter", js: true do
+  describe "sorter" do
     describe "by weight" do
-      it "sorts libraries by weight"
+      it "sorts libraries by weight" do
+        click_link "Popularity"
+        click_link "Weight"
+
+        Library.by_weight.zip(library_items).each do |library, library_item|
+          expect(library_item.find('span.info')).to have_text library.name
+        end
+      end
     end
 
     describe "by popularity" do
-      it "sorts libraries by popularity"
+      it "sorts libraries by popularity" do
+        click_link "Popularity"
+
+        Library.by_popularity.zip(library_items).each do |library, library_item|
+          expect(library_item.find('span.info')).to have_text library.name
+        end
+      end
+    end
+
+    context "with filter" do
+      describe "by popularity" do
+        it "sorts filtered libraries by popularity" do
+          category = random_category
+
+          click_link category.name
+          click_link "Popularity"
+
+          Library.where(category: category)
+                 .by_popularity
+                 .zip(library_items)
+                 .each do |library, library_item|
+            expect(library_item.find('span.info')).to have_text library.name
+          end
+        end
+      end
     end
   end
 
-  describe "searcher", js: true do
+  describe "searcher" do
     it "searches libraries by name"
   end
 
-  describe "pager", js: true do
+  describe "pager" do
 
   end
 end
