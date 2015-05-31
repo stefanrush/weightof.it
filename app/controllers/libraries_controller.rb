5cc757b03d4bde039a3cff347fac7fed75ce4b51
Stefan Rush <stefanrush@gmail.com>
2015-05-30 22:44:52 -0400

    Add controller tests for libraries

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -2,6 +2,8 @@ class LibrariesController < ApplicationController
   def index
     @libraries  = Library.active
     @categories = Category.by_position
-    @category   = @categories.find_by_slug(params[:slug]) if params[:slug]
+    @category   = @categories.find_by_slug!(params[:slug]) if params[:slug]
+  rescue ActiveRecord::RecordNotFound
+    render_404
   end
 end
