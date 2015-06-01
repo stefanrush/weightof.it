fc283de840f9d4ce355ad1d1378cab04e413652e
Stefan Rush <stefanrush@gmail.com>
2015-06-01 01:28:15 -0400

    Add feature specs for searcher

--- spec/features/libraries_spec.rb
+++ spec/features/libraries_spec.rb
@@ -5,13 +5,21 @@ RSpec.describe "Libraries", type: :feature, js: true do
     Category.all[rand(Category.count)]
   end
 
+  def search_results(query, libraries=Library)
+    libraries.where('name LIKE ?', "%#{query}%")
+  end
+
+  def trigger_search
+    page.driver.execute_script "$('#search').trigger('keyup');"
+  end
+
   def library_items
     all('section.libraries ol.list li')
   end
 
   before do
-    3.times  { create(:category) }
-    10.times { create(:library, category: random_category) }
+    4.times  { create(:category) }
+    20.times { create(:library, category: random_category) }
     visit root_path
   end
 
@@ -21,10 +29,12 @@ RSpec.describe "Libraries", type: :feature, js: true do
         Category.by_position.each do |category|
           library_count = Library.where(category: category).count
           click_link category.name
+          
           expect(library_items.size).to eq(library_count)
         end
 
         click_link "All Libraries"
+        
         expect(library_items.size).to eq(Library.count)
       end
 
@@ -40,6 +50,21 @@ RSpec.describe "Libraries", type: :feature, js: true do
                  .zip(library_items)
                  .each do |library, library_item|
             expect(library_item.find('span.info')).to have_text(library.name)
+          end
+        end
+      end
+      
+      context "with search" do
+        it "filters searched libraries by category" do
+          query = "1"
+          fill_in :search, with: query
+          trigger_search
+
+          Category.by_position.each do |category|
+            click_link category.name
+            libraries = search_results(query, Library.where(category: category))
+            
+            expect(library_items.size).to eq(libraries.count)
           end
         end
       end
@@ -70,7 +95,13 @@ RSpec.describe "Libraries", type: :feature, js: true do
   end
 
   describe "searcher" do
-    #it "searches libraries by name"
+    it "searches libraries by name" do
+      query = "1"
+      fill_in :search, with: query
+      trigger_search
+
+      expect(library_items.size).to eq(search_results(query).count)
+    end
   end
 
   describe "pager" do
