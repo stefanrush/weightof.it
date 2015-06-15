65bcaa01b55a5b0771e6d3a4a5469092f211ec2f
Stefan Rush <stefanrush@gmail.com>
2015-06-15 08:02:22 -0400

    Annotate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# == Route Map
+#
+#     Prefix Verb URI Pattern               Controller#Action
+#       root GET  /                         libraries#index
+#            GET  /category/:slug(.:format) libraries#index
+# contribute GET  /contribute(.:format)     libraries#new
+#  libraries POST /contribute(.:format)     libraries#create
+#
+
 Rails.application.routes.draw do
   root                   to: 'libraries#index'
   get  'category/:slug', to: 'libraries#index'
