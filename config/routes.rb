3e406d4abae9c123e749c156b8e39b0e1a76d357
Stefan Rush <stefanrush@gmail.com>
2015-06-19 18:57:16 -0400

    Add custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   get  'category/:slug', to: 'libraries#index'
   get  'contribute',     to: 'libraries#new'
   post 'contribute',     to: 'libraries#create', as: :libraries
+
+  match '/404', to: 'errors#not_found', via: :all
+  match '/422', to: 'errors#unprocessable_entity', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
