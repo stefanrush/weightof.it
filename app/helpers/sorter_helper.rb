c4509c4a30ecb6882080f7203068c4f4fd5c3275
Stefan Rush <stefanrush@gmail.com>
2015-06-22 11:47:49 -0400

    Add sort link titles

--- app/helpers/sorter_helper.rb
+++ app/helpers/sorter_helper.rb
@@ -1,9 +1,10 @@
 module SorterHelper
-  # Accepts sort name (e.g. weight, popularity)
+  # Accepts sort name (e.g. weight, popularity) and a link title
   # Returns sort link element
-  def sort_link(sort)
+  def sort_link(sort, title=nil)
     link_to sort.capitalize, build_url(sort: sort),
                              class: sort == @sort ? 'active' : nil,
+                             title: title,
                              data: { sort: sort }
   end
 end
